faf12e8a7a86f8de31cc3ebdecfbd4501b895405
willian-alves <willian.alves@turingbits.com.br>
2025-06-22 15:17:08 -0300

    Projeto Criado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { SignupComponent } from './page/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login", component: LoginComponent
+    },
+    {
+        path: "signup", component: SignupComponent
+    }
+];
